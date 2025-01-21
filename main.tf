fcbbaf238680693ce0d784b6d4378aa5abc7d7a6
mzwandilemkhokha <mzwandilemkhokha@yahoo.com>
2025-01-21 15:32:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
 
+}
+
+############ Creating a Random String ############
+resource "random_string" "random" {
+  length = 6
+  special = false
+  upper = false
+} 
+############ Creating an S3 Bucket ############ 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "whizbucket-${random_string.random.result}"
+  force_destroy = true
+}
